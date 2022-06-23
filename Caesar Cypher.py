2f36469fea679194923d0a5da2fabe1354a98d9f
Dbhattach15 <107955731+Dbhattach15@users.noreply.github.com>
2022-06-23 19:08:23 -0400

    Add files via upload

---
+++ Caesar Cypher.py
@@ -0,0 +1,27 @@
+from alphabet import letters
+from Defined import caesar
+
+
+
+
+print("WELCOME TO THE CAESAR CIPHER")
+
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+    
+
+
